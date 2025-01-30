b256564741850eb84f5a9e071fc0f6b216b4b743
pembe <pembe@ADMIN8PEMBELIAN>
2025-01-30 09:29:44 +0700

    changing from xls to csv with ; as delimiter and read csv

--- serve.js
+++ serve.js
@@ -1,62 +1,65 @@
-const express = require('express')
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-const fileFilter = (req, file, cb)=>{
-    const allowedMimeTypes = ["application/vnd.ms-excel"]
+const {parse} = require('csv-parse/sync')
 
-    if(allowedMimeTypes.includes(file.mimetype)){
-        cb(null, true);
-    }else{
-        cb(new Error('File is not supported'), false);
-    }
-}
+const fileFilter = (req, file, cb) => {
+  const allowedMimeTypes = ["text/csv"];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("File is not supported"), false);
+  }
+};
 
-const uploadstor = multer({storage: multer.memoryStorage(), fileFilter: fileFilter})
+const uploadstor = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: fileFilter,
+});
 
-const app = express()
+const app = express();
 
+app.set("view engine", "ejs");
 
-app.set("view engine", "ejs")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use("/public", express.static("public"));
 
-app.use("/public", express.static('public'))
+app.get("/", (req, res) => {
+  res.render("user_interface");
+});
 
-app.get('/', (req,res)=>{
-    res.render("user_interface")
-})
+app.post("/generate", uploadstor.single("file"), (req, res, next) => {
+  if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads");
+  }
 
-app.post('/generate', ()=>{
-    if(false){
-        uploadstor.single('file')
-    }
-}, (req,res,next) => {
+  const records = parse(req.file.buffer.toString('utf-8'), {
+    delimiter: ';',
+    columns: true, 
+    skip_empty_lines: true,
+  })
+  
+  if (!req.file) {
+    return res.status(400).json({ message: "file not found" });
+  }
+  console.log(records)
+  res.status(200).json({ message: "file uploaded successfully" });
+});
 
-    if(!fs.existsSync('uploads')){
-        fs.mkdirSync('uploads')
-    }
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).send({ message: "Multer error: " + err.message });
+  }
+  if (err) {
+    return res.status(400).send({ message: err.message });
+  }
+  next();
+});
 
-    if(!req.file){
-        return res.status(400).json({ message: "file not found"})
-    }
-
-    app.use(express.json)
-    res.status(200).json({ message: "file uploaded successfully" })
-})
-
-app.use((err, req, res, next)=>{
-    if (err instanceof multer.MulterError){
-        return res.status(400).send({message: "Multer error: " + err.message});
-    } 
-    if (err) {
-        return res.status(400).send({ message: err.message })
-    }
-    next();
-})
-
-app.listen(3000, 'localhost', () => {
-    console.log('listening to http://localhost:3000')
-})
+app.listen(3000, "localhost", () => {
+  console.log("listening to http://localhost:3000");
+});
